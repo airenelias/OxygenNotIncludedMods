986f4ec0e9e07ec2ab4c59d76b9901cebfd188a8
Airen <airenilia@gmail.com>
2025-08-10 15:33:58 +0700

    Add enriched uranium to buildables

--- RefinedMetalsAreStillMetals/RefinedMetalsAreStillMetalsPatch.cs
+++ RefinedMetalsAreStillMetals/RefinedMetalsAreStillMetalsPatch.cs
@@ -21,11 +21,16 @@ namespace RefinedMetalsAreStillMetals
         {
             public static void Postfix()
             {
+                // make all refined metals usable as ore
                 foreach (Element element in ElementLoader.elements.FindAll((Predicate<Element>)
                     (e => e.IsSolid && e.HasTag(GameTags.RefinedMetal) && !e.HasTag(GameTags.Metal))))
                 {
                     element.oreTags = element.oreTags.Append(GameTags.Metal);
                 }
+
+                // make enriched uranium usable as refined and ore
+                Element enrichedUranium = ElementLoader.FindElementByHash(SimHashes.EnrichedUranium);
+                enrichedUranium.oreTags = enrichedUranium.oreTags.Append(new[] { GameTags.RefinedMetal, GameTags.Metal });
             }
         }
     }
